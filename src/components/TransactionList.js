097eace5702cb76bf29b5a0137f364e7814fde72
Haq Nawaz <haqnawaz99@yahoo.com>
2020-06-12 22:16:33 +0500

    UI till Add Transactions

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -6,8 +6,8 @@ export const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        <li class="minus">
-        Cash <span>-$400</span><button class="delete-btn">x</button>
+        <li className="minus">
+        Cash <span>-$400</span><button className="delete-btn">x</button>
         </li>
         </ul>
     </>
