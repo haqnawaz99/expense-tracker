67abacd7819305b0147d940a169245fbb1d2f74f
Haq Nawaz <haqnawaz99@yahoo.com>
2020-06-12 21:24:00 +0500

    Header Component and CSS
    Header Component and CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import { Header } from './components/Header'
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World
-      <br/>
-      Just checking Actions
+      <Header/>
     </div>
   );
 }
