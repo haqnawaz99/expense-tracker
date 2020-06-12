a32e16b55992c8209d90e8edb4f8914b56e93c7d
Haq Nawaz <haqnawaz99@yahoo.com>
2020-06-13 01:40:29 +0500

    final let see

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,4 +1,5 @@
 import React , { useContext } from 'react';
+import { Transaction } from './Transaction'
 import { GlobalContext } from '../context/GlobalState';
 
 
@@ -9,9 +10,8 @@ export const TransactionList = () => {
     <> 
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transactions =>(<li className="minus">
-        {transactions.text} <span>-$400</span><button className="delete-btn">x</button>
-        </li>))}
+        {transactions.map(transaction =>(<Transaction key = {transaction.id} transaction = 
+        {transaction}/>))}
         
         </ul>
     </>
